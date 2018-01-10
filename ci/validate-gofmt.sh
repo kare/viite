97c6f012999b8a66df063309f71b1a4a99b5145e
Kare Nuorteva <kare.nuorteva@me.com>
2018-01-10 21:21:36 +0200

    Validate gofmt has been run before commit

--- ci/validate-gofmt.sh
+++ ci/validate-gofmt.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # $pkg is relative path to package
-pkg=.
+pkg=kkn.fi/viite
+importPath=kkn.fi/viite
+relativePkg="${pkg/$importPath/.}"
 
-gofmt -s -l $pkg
-if [ $? -ne 0 ]; then
+output=`gofmt -s -l $relativePkg`
+if [ "$output" != "" ]; then
 	echo "validate-gofmt.sh: error $pkg" 1>&2
 	exit 1
 fi
